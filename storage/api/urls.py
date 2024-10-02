f8db2dc3944ecc44030ca27224f0a977b28fb109
MaryChrispinaNjuguna <chrispinamary@gmail.com>
2024-10-02 23:57:19 +0300

    modified comments in urls.py

--- storage/api/urls.py
+++ storage/api/urls.py
@@ -8,14 +8,11 @@ urlpatterns = [
     path('location/', LocationList.as_view()),
 
     # URL pattern for retrieving, updating, or deleting a specific location by its primary key (pk)
-    # Maps to the LocationDetail view (GET, PUT, PATCH, DELETE requests)
     path('location/<int:pk>/', LocationDetail.as_view()),
 
     # URL pattern for listing all items or creating a new item
-    # Maps to the ItemList view (GET and POST requests)
     path('item/', ItemList.as_view()),
 
     # URL pattern for retrieving, updating, or deleting a specific item by its primary key (pk)
-    # Maps to the ItemDetail view (GET, PUT, PATCH, DELETE requests)
     path('item/<int:pk>/', ItemDetail.as_view()),
 ]
